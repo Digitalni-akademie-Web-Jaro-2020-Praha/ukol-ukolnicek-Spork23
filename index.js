8013950d7bc28f6e614e9a7488bfdb66abdba070
Spork23 <55710898+Spork23@users.noreply.github.com>
2020-05-14 21:18:36 +0200

    prvá až tretie časť hotová

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict';
+const tasks = ["Vyprat ponožky", "Naučit se funkce vyššího řádu", "Nakoupit na víkend"];
+const updateTasks = () => {
+  const HTML = document.querySelector(".todo__tasks");
+  for (let i = 0; i < tasks.length; i++) {
+    HTML.innerHTML += `<div class="task"> ${tasks[i]} </div>`;
+  }
+};
+updateTasks();
+
+
 
 console.log('funguju!');
