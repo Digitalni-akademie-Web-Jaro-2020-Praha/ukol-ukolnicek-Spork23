89f433e333e060b34c61fd57e62418069f99664b
Spork23 <55710898+Spork23@users.noreply.github.com>
2020-05-15 13:18:16 +0200

    stvrtá čast pridaná

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 'use strict';
 const tasks = ["Vyprat ponožky", "Naučit se funkce vyššího řádu", "Nakoupit na víkend"];
+
 const updateTasks = () => {
   const HTML = document.querySelector(".todo__tasks");
+  HTML.innerHTML = "";
   for (let i = 0; i < tasks.length; i++) {
     HTML.innerHTML += `<div class="task"> ${tasks[i]} </div>`;
   }
 };
 updateTasks();
 
+const update2 = () => {
+  const inputField = document.querySelector("#new-task");
+  tasks.push(inputField.value);
+  console.log("whatever");
+  updateTasks();
+}
 
+document.querySelector(".btn-add").addEventListener("click", update2);
 
 console.log('funguju!');
